101d2aa15b4980c420859a79138f53f4c8499d7a
dfazage <fazabinmus@gmail.com>
2020-10-24 01:05:15 +0200

    lesson 3 files commit

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:smoge/ui/home/widgets/animated_percentage_widget.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _buildTitle(),
+            _buildAirPollutionContent(),
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+Widget _buildTitle() => Text(
+  "Wroclaw",
+  textAlign: TextAlign.center,
+);
+
+Widget _buildAirPollutionContent() => Expanded(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      AnimatedPercentageWidget(
+        fromValue: 0,
+        toValue: 310,
+      ),
+      Text("norm"),
+    ],
+  ),
+);
